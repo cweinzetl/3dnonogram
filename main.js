e07f6c5749cb85c5b18749ef6cf2fddbdf3bd8b4
Carsen Weinzetl <ctw7@posteo.net>
2024-09-10 22:22:59 +0000

    add fetchAndParseJson

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 
 // Scene setup
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
@@ -26,20 +27,45 @@ renderer.setAnimationLoop( animate );
 var puzzleDataPath = 'public/puzzles/test.json'; // placeholder
 
 // Function to decode RLE and create voxels
-function createVoxelsFromPuzzleData(puzzleDataPath) {
+function createPuzzleFromData(puzzleDataPath) {
     // Fetch and parse the JSON data
-    fetch(puzzleDataPath)
-        .then(response => response.json())
-        .then(data => {
-        console.log('Puzzle data loaded: ${name}', data);
-            
-        /* TODO
-            - Convert RLE to bitmap
-            - Use puzzleData and bitmap to create puzzle 
-        */
+    async function fetchAndParseJson() {
+        // Try to getch JSON from given path
+        try {
+            const res = await fetch(puzzleDataPath);
+            if (!res.ok) {
+                throw new Error('Response status: $(response.status');
+            }
+            const json = res.json();
+            console.log(json);
+        } catch (error) {
+            console.error(error.message);
+        }
 
-        })
-        .catch(error => console.error('Error loading puzzle:', error));
+        // TODO: Instantiate data from JSON
+        
+    }
+
+        /* //Convert rle to bitmap
+        function rleToBitmap(rle, width, height) {
+            // Initialize an empty array to hold the bitmap
+            const bitmap = new Array(width * height).fill(0);
+            let index = 0;
+        
+            // Loop through the rle data
+            for (let i = 0; i < rle.length; i += 2) {
+                const value = rle[i];
+                const count = rle[i + 1];
+                // Fill the bitmap with the current value for the specified count
+                for (let j = 0; j < count; j++) {
+                    if (index < bitmap.length) {
+                        bitmap[index] = value;
+                        index++;
+                    }
+                }
+            }
+        } 
+        */
 }
 
-  createVoxelsFromPuzzleData();
+  createPuzzleFromData();
